327df44bcc5b68007c95c87c957e99e6128b823c
karimabdelaal7-collab <karim.abdelaal7@gmail.com>
2026-08-06 11:35:51 +0300

    Add files via upload

---
+++ products-lingerie.js
@@ -0,0 +1,624 @@
+// بيانات منتجات قسم لانجري — أسماء فولدرات مميزة (l01-l62) لتفادي التصادم
+// ⚠️ الاسم والسعر placeholder مؤقت — هيتملى من شيت الأونر
+const lingerieProducts = [
+  {
+    id: "lingerie-01",
+    code: "429", // كود المنتج الأصلي (للمطابقة مع شيت الأونر)
+    nameAr: "", // TODO: يتملى من الشيت
+    nameEn: "", // TODO
+    price: 0, // TODO
+    size: "",
+    description: "",
+    images: ["l01/1.jpg", "l01/2.jpg", "l01/3.jpg"]
+  },
+  {
+    id: "lingerie-02",
+    code: "375", // كود المنتج الأصلي (للمطابقة مع شيت الأونر)
+    nameAr: "", // TODO: يتملى من الشيت
+    nameEn: "", // TODO
+    price: 0, // TODO
+    size: "",
+    description: "",
+    images: ["l02/1.jpg", "l02/2.jpg"]
+  },
+  {
+    id: "lingerie-03",
+    code: "402", // كود المنتج الأصلي (للمطابقة مع شيت الأونر)
+    nameAr: "", // TODO: يتملى من الشيت
+    nameEn: "", // TODO
+    price: 0, // TODO
+    size: "",
+    description: "",
+    images: ["l03/1.jpg"]
+  },
+  {
+    id: "lingerie-04",
+    code: "366", // كود المنتج الأصلي (للمطابقة مع شيت الأونر)
+    nameAr: "", // TODO: يتملى من الشيت
+    nameEn: "", // TODO
+    price: 0, // TODO
+    size: "",
+    description: "",
+    images: ["l04/1.jpg", "l04/2.jpg", "l04/3.jpg", "l04/4.jpg", "l04/5.jpg"]
+  },
+  {
+    id: "lingerie-05",
+    code: "403", // كود المنتج الأصلي (للمطابقة مع شيت الأونر)
+    nameAr: "", // TODO: يتملى من الشيت
+    nameEn: "", // TODO
+    price: 0, // TODO
+    size: "",
+    description: "",
+    images: ["l05/1.jpg", "l05/2.jpg", "l05/3.jpg", "l05/4.jpg"]
+  },
+  {
+    id: "lingerie-06",
+    code: "301", // كود المنتج الأصلي (للمطابقة مع شيت الأونر)
+    nameAr: "", // TODO: يتملى من الشيت
+    nameEn: "", // TODO
+    price: 0, // TODO
+    size: "",
+    description: "",
+    images: ["l06/1.jpg", "l06/2.jpg"]
+  },
+  {
+    id: "lingerie-07",
+    code: "365", // كود المنتج الأصلي (للمطابقة مع شيت الأونر)
+    nameAr: "", // TODO: يتملى من الشيت
+    nameEn: "", // TODO
+    price: 0, // TODO
+    size: "",
+    description: "",
+    images: ["l07/1.jpg", "l07/2.jpg"]
+  },
+  {
+    id: "lingerie-08",
+    code: "310", // كود المنتج الأصلي (للمطابقة مع شيت الأونر)
+    nameAr: "", // TODO: يتملى من الشيت
+    nameEn: "", // TODO
+    price: 0, // TODO
+    size: "",
+    description: "",
+    images: ["l08/1.jpg", "l08/2.jpg", "l08/3.jpg"]
+  },
+  {
+    id: "lingerie-09",
+    code: "200", // كود المنتج الأصلي (للمطابقة مع شيت الأونر)
+    nameAr: "", // TODO: يتملى من الشيت
+    nameEn: "", // TODO
+    price: 0, // TODO
+    size: "",
+    description: "",
+    images: ["l09/1.jpg", "l09/2.jpg", "l09/3.jpg", "l09/4.jpg"]
+  },
+  {
+    id: "lingerie-10",
+    code: "110", // كود المنتج الأصلي (للمطابقة مع شيت الأونر)
+    nameAr: "", // TODO: يتملى من الشيت
+    nameEn: "", // TODO
+    price: 0, // TODO
+    size: "",
+    description: "",
+    images: ["l10/1.jpg", "l10/2.jpg"]
+  },
+  {
+    id: "lingerie-11",
+    code: "230", // كود المنتج الأصلي (للمطابقة مع شيت الأونر)
+    nameAr: "", // TODO: يتملى من الشيت
+    nameEn: "", // TODO
+    price: 0, // TODO
+    size: "",
+    description: "",
+    images: ["l11/1.jpg", "l11/2.jpg"]
+  },
+  {
+    id: "lingerie-12",
+    code: "229", // كود المنتج الأصلي (للمطابقة مع شيت الأونر)
+    nameAr: "", // TODO: يتملى من الشيت
+    nameEn: "", // TODO
+    price: 0, // TODO
+    size: "",
+    description: "",
+    images: ["l12/1.jpg", "l12/2.jpg", "l12/3.jpg", "l12/4.jpg"]
+  },
+  {
+    id: "lingerie-13",
+    code: "231", // كود المنتج الأصلي (للمطابقة مع شيت الأونر)
+    nameAr: "", // TODO: يتملى من الشيت
+    nameEn: "", // TODO
+    price: 0, // TODO
+    size: "",
+    description: "",
+    images: ["l13/1.jpg", "l13/2.jpg"]
+  },
+  {
+    id: "lingerie-14",
+    code: "002", // كود المنتج الأصلي (للمطابقة مع شيت الأونر)
+    nameAr: "", // TODO: يتملى من الشيت
+    nameEn: "", // TODO
+    price: 0, // TODO
+    size: "",
+    description: "",
+    images: ["l14/1.jpg", "l14/2.jpg"]
+  },
+  {
+    id: "lingerie-15",
+    code: "2404", // كود المنتج الأصلي (للمطابقة مع شيت الأونر)
+    nameAr: "", // TODO: يتملى من الشيت
+    nameEn: "", // TODO
+    price: 0, // TODO
+    size: "",
+    description: "",
+    images: ["l15/1.jpg", "l15/2.jpg", "l15/3.jpg"]
+  },
+  {
+    id: "lingerie-16",
+    code: "2446", // كود المنتج الأصلي (للمطابقة مع شيت الأونر)
+    nameAr: "", // TODO: يتملى من الشيت
+    nameEn: "", // TODO
+    price: 0, // TODO
+    size: "",
+    description: "",
+    images: ["l16/1.jpg", "l16/2.jpg"]
+  },
+  {
+    id: "lingerie-17",
+    code: "2298", // كود المنتج الأصلي (للمطابقة مع شيت الأونر)
+    nameAr: "", // TODO: يتملى من الشيت
+    nameEn: "", // TODO
+    price: 0, // TODO
+    size: "",
+    description: "",
+    images: ["l17/1.jpg", "l17/2.jpg"]
+  },
+  {
+    id: "lingerie-18",
+    code: "2470", // كود المنتج الأصلي (للمطابقة مع شيت الأونر)
+    nameAr: "", // TODO: يتملى من الشيت
+    nameEn: "", // TODO
+    price: 0, // TODO
+    size: "",
+    description: "",
+    images: ["l18/1.jpg", "l18/2.jpg", "l18/3.jpg"]
+  },
+  {
+    id: "lingerie-19",
+    code: "2507", // كود المنتج الأصلي (للمطابقة مع شيت الأونر)
+    nameAr: "", // TODO: يتملى من الشيت
+    nameEn: "", // TODO
+    price: 0, // TODO
+    size: "",
+    description: "",
+    images: ["l19/1.jpg", "l19/2.jpg", "l19/3.jpg", "l19/4.jpg", "l19/5.jpg"]
+  },
+  {
+    id: "lingerie-20",
+    code: "4009", // كود المنتج الأصلي (للمطابقة مع شيت الأونر)
+    nameAr: "", // TODO: يتملى من الشيت
+    nameEn: "", // TODO
+    price: 0, // TODO
+    size: "",
+    description: "",
+    images: ["l20/1.jpg", "l20/2.jpg", "l20/3.jpg", "l20/4.jpg"]
+  },
+  {
+    id: "lingerie-21",
+    code: "4011", // كود المنتج الأصلي (للمطابقة مع شيت الأونر)
+    nameAr: "", // TODO: يتملى من الشيت
+    nameEn: "", // TODO
+    price: 0, // TODO
+    size: "",
+    description: "",
+    images: ["l21/1.jpg", "l21/2.jpg"]
+  },
+  {
+    id: "lingerie-22",
+    code: "4012", // كود المنتج الأصلي (للمطابقة مع شيت الأونر)
+    nameAr: "", // TODO: يتملى من الشيت
+    nameEn: "", // TODO
+    price: 0, // TODO
+    size: "",
+    description: "",
+    images: ["l22/1.jpg", "l22/2.jpg", "l22/3.jpg", "l22/4.jpg", "l22/5.jpg"]
+  },
+  {
+    id: "lingerie-23",
+    code: "5004", // كود المنتج الأصلي (للمطابقة مع شيت الأونر)
+    nameAr: "", // TODO: يتملى من الشيت
+    nameEn: "", // TODO
+    price: 0, // TODO
+    size: "",
+    description: "",
+    images: ["l23/1.jpg", "l23/2.jpg"]
+  },
+  {
+    id: "lingerie-24",
+    code: "5007", // كود المنتج الأصلي (للمطابقة مع شيت الأونر)
+    nameAr: "", // TODO: يتملى من الشيت
+    nameEn: "", // TODO
+    price: 0, // TODO
+    size: "",
+    description: "",
+    images: ["l24/1.jpg", "l24/2.jpg"]
+  },
+  {
+    id: "lingerie-25",
+    code: "5008", // كود المنتج الأصلي (للمطابقة مع شيت الأونر)
+    nameAr: "", // TODO: يتملى من الشيت
+    nameEn: "", // TODO
+    price: 0, // TODO
+    size: "",
+    description: "",
+    images: ["l25/1.jpg"]
+  },
+  {
+    id: "lingerie-26",
+    code: "383", // كود المنتج الأصلي (للمطابقة مع شيت الأونر)
+    nameAr: "", // TODO: يتملى من الشيت
+    nameEn: "", // TODO
+    price: 0, // TODO
+    size: "",
+    description: "",
+    images: ["l26/1.jpg", "l26/2.jpg", "l26/3.jpg", "l26/4.jpg"]
+  },
+  {
+    id: "lingerie-27",
+    code: "404", // كود المنتج الأصلي (للمطابقة مع شيت الأونر)
+    nameAr: "", // TODO: يتملى من الشيت
+    nameEn: "", // TODO
+    price: 0, // TODO
+    size: "",
+    description: "",
+    images: ["l27/1.jpg", "l27/2.jpg", "l27/3.jpg", "l27/4.jpg", "l27/5.jpg", "l27/6.jpg", "l27/7.jpg"]
+  },
+  {
+    id: "lingerie-28",
+    code: "2018", // كود المنتج الأصلي (للمطابقة مع شيت الأونر)
+    nameAr: "", // TODO: يتملى من الشيت
+    nameEn: "", // TODO
+    price: 0, // TODO
+    size: "",
+    description: "",
+    images: ["l28/1.jpg", "l28/2.jpg", "l28/3.jpg", "l28/4.jpg"]
+  },
+  {
+    id: "lingerie-29",
+    code: "2013", // كود المنتج الأصلي (للمطابقة مع شيت الأونر)
+    nameAr: "", // TODO: يتملى من الشيت
+    nameEn: "", // TODO
+    price: 0, // TODO
+    size: "",
+    description: "",
+    images: ["l29/1.jpg", "l29/2.jpg", "l29/3.jpg", "l29/4.jpg", "l29/5.jpg", "l29/6.jpg"]
+  },
+  {
+    id: "lingerie-30",
+    code: "2022", // كود المنتج الأصلي (للمطابقة مع شيت الأونر)
+    nameAr: "", // TODO: يتملى من الشيت
+    nameEn: "", // TODO
+    price: 0, // TODO
+    size: "",
+    description: "",
+    images: ["l30/1.jpg", "l30/2.jpg", "l30/3.jpg", "l30/4.jpg"]
+  },
+  {
+    id: "lingerie-31",
+    code: "441", // كود المنتج الأصلي (للمطابقة مع شيت الأونر)
+    nameAr: "", // TODO: يتملى من الشيت
+    nameEn: "", // TODO
+    price: 0, // TODO
+    size: "",
+    description: "",
+    images: ["l31/1.jpg", "l31/2.jpg"]
+  },
+  {
+    id: "lingerie-32",
+    code: "446", // كود المنتج الأصلي (للمطابقة مع شيت الأونر)
+    nameAr: "", // TODO: يتملى من الشيت
+    nameEn: "", // TODO
+    price: 0, // TODO
+    size: "",
+    description: "",
+    images: ["l32/1.jpg", "l32/2.jpg", "l32/3.jpg", "l32/4.jpg"]
+  },
+  {
+    id: "lingerie-33",
+    code: "445", // كود المنتج الأصلي (للمطابقة مع شيت الأونر)
+    nameAr: "", // TODO: يتملى من الشيت
+    nameEn: "", // TODO
+    price: 0, // TODO
+    size: "",
+    description: "",
+    images: ["l33/1.jpg", "l33/2.jpg", "l33/3.jpg", "l33/4.jpg", "l33/5.jpg"]
+  },
+  {
+    id: "lingerie-34",
+    code: "432", // كود المنتج الأصلي (للمطابقة مع شيت الأونر)
+    nameAr: "", // TODO: يتملى من الشيت
+    nameEn: "", // TODO
+    price: 0, // TODO
+    size: "",
+    description: "",
+    images: ["l34/1.jpg", "l34/2.jpg", "l34/3.jpg", "l34/4.jpg", "l34/5.jpg"]
+  },
+  {
+    id: "lingerie-35",
+    code: "401", // كود المنتج الأصلي (للمطابقة مع شيت الأونر)
+    nameAr: "", // TODO: يتملى من الشيت
+    nameEn: "", // TODO
+    price: 0, // TODO
+    size: "",
+    description: "",
+    images: ["l35/1.jpg", "l35/2.jpg", "l35/3.jpg", "l35/4.jpg"]
+  },
+  {
+    id: "lingerie-36",
+    code: "391", // كود المنتج الأصلي (للمطابقة مع شيت الأونر)
+    nameAr: "", // TODO: يتملى من الشيت
+    nameEn: "", // TODO
+    price: 0, // TODO
+    size: "",
+    description: "",
+    images: ["l36/1.jpg"]
+  },
+  {
+    id: "lingerie-37",
+    code: "442", // كود المنتج الأصلي (للمطابقة مع شيت الأونر)
+    nameAr: "", // TODO: يتملى من الشيت
+    nameEn: "", // TODO
+    price: 0, // TODO
+    size: "",
+    description: "",
+    images: ["l37/1.jpg", "l37/2.jpg"]
+  },
+  {
+    id: "lingerie-38",
+    code: "398", // كود المنتج الأصلي (للمطابقة مع شيت الأونر)
+    nameAr: "", // TODO: يتملى من الشيت
+    nameEn: "", // TODO
+    price: 0, // TODO
+    size: "",
+    description: "",
+    images: ["l38/1.jpg"]
+  },
+  {
+    id: "lingerie-39",
+    code: "196", // كود المنتج الأصلي (للمطابقة مع شيت الأونر)
+    nameAr: "", // TODO: يتملى من الشيت
+    nameEn: "", // TODO
+    price: 0, // TODO
+    size: "",
+    description: "",
+    images: ["l39/1.jpg", "l39/2.jpg", "l39/3.jpg"]
+  },
+  {
+    id: "lingerie-40",
+    code: "221", // كود المنتج الأصلي (للمطابقة مع شيت الأونر)
+    nameAr: "", // TODO: يتملى من الشيت
+    nameEn: "", // TODO
+    price: 0, // TODO
+    size: "",
+    description: "",
+    images: ["l40/1.jpg", "l40/2.jpg"]
+  },
+  {
+    id: "lingerie-41",
+    code: "430", // كود المنتج الأصلي (للمطابقة مع شيت الأونر)
+    nameAr: "", // TODO: يتملى من الشيت
+    nameEn: "", // TODO
+    price: 0, // TODO
+    size: "",
+    description: "",
+    images: ["l41/1.jpg", "l41/2.jpg"]
+  },
+  {
+    id: "lingerie-42",
+    code: "409", // كود المنتج الأصلي (للمطابقة مع شيت الأونر)
+    nameAr: "", // TODO: يتملى من الشيت
+    nameEn: "", // TODO
+    price: 0, // TODO
+    size: "",
+    description: "",
+    images: ["l42/1.jpg", "l42/2.jpg"]
+  },
+  {
+    id: "lingerie-43",
+    code: "158", // كود المنتج الأصلي (للمطابقة مع شيت الأونر)
+    nameAr: "", // TODO: يتملى من الشيت
+    nameEn: "", // TODO
+    price: 0, // TODO
+    size: "",
+    description: "",
+    images: ["l43/1.jpg"]
+  },
+  {
+    id: "lingerie-44",
+    code: "448", // كود المنتج الأصلي (للمطابقة مع شيت الأونر)
+    nameAr: "", // TODO: يتملى من الشيت
+    nameEn: "", // TODO
+    price: 0, // TODO
+    size: "",
+    description: "",
+    images: ["l44/1.jpg", "l44/2.jpg", "l44/3.jpg", "l44/4.jpg", "l44/5.jpg", "l44/6.jpg", "l44/7.jpg", "l44/8.jpg"]
+  },
+  {
+    id: "lingerie-45",
+    code: "385", // كود المنتج الأصلي (للمطابقة مع شيت الأونر)
+    nameAr: "", // TODO: يتملى من الشيت
+    nameEn: "", // TODO
+    price: 0, // TODO
+    size: "",
+    description: "",
+    images: ["l45/1.jpg", "l45/2.jpg", "l45/3.jpg", "l45/4.jpg"]
+  },
+  {
+    id: "lingerie-46",
+    code: "502", // كود المنتج الأصلي (للمطابقة مع شيت الأونر)
+    nameAr: "", // TODO: يتملى من الشيت
+    nameEn: "", // TODO
+    price: 0, // TODO
+    size: "",
+    description: "",
+    images: ["l46/1.jpg", "l46/2.jpg", "l46/3.jpg"]
+  },
+  {
+    id: "lingerie-47",
+    code: "368", // كود المنتج الأصلي (للمطابقة مع شيت الأونر)
+    nameAr: "", // TODO: يتملى من الشيت
+    nameEn: "", // TODO
+    price: 0, // TODO
+    size: "",
+    description: "",
+    images: ["l47/1.jpg", "l47/2.jpg", "l47/3.jpg", "l47/4.jpg", "l47/5.jpg", "l47/6.jpg", "l47/7.jpg", "l47/8.jpg", "l47/9.jpg"]
+  },
+  {
+    id: "lingerie-48",
+    code: "501", // كود المنتج الأصلي (للمطابقة مع شيت الأونر)
+    nameAr: "", // TODO: يتملى من الشيت
+    nameEn: "", // TODO
+    price: 0, // TODO
+    size: "",
+    description: "",
+    images: ["l48/1.jpg", "l48/2.jpg", "l48/3.jpg", "l48/4.jpg", "l48/5.jpg"]
+  },
+  {
+    id: "lingerie-49",
+    code: "407", // كود المنتج الأصلي (للمطابقة مع شيت الأونر)
+    nameAr: "", // TODO: يتملى من الشيت
+    nameEn: "", // TODO
+    price: 0, // TODO
+    size: "",
+    description: "",
+    images: ["l49/1.jpg", "l49/2.jpg", "l49/3.jpg", "l49/4.jpg", "l49/5.jpg", "l49/6.jpg", "l49/7.jpg", "l49/8.jpg", "l49/9.jpg", "l49/10.jpg"]
+  },
+  {
+    id: "lingerie-50",
+    code: "503", // كود المنتج الأصلي (للمطابقة مع شيت الأونر)
+    nameAr: "", // TODO: يتملى من الشيت
+    nameEn: "", // TODO
+    price: 0, // TODO
+    size: "",
+    description: "",
+    images: ["l50/1.jpg", "l50/2.jpg", "l50/3.jpg", "l50/4.jpg", "l50/5.jpg", "l50/6.jpg", "l50/7.jpg", "l50/8.jpg", "l50/9.jpg", "l50/10.jpg"]
+  },
+  {
+    id: "lingerie-51",
+    code: "230", // كود المنتج الأصلي (للمطابقة مع شيت الأونر)
+    nameAr: "", // TODO: يتملى من الشيت
+    nameEn: "", // TODO
+    price: 0, // TODO
+    size: "",
+    description: "",
+    images: ["l51/1.jpg", "l51/2.jpg", "l51/3.jpg", "l51/4.jpg", "l51/5.jpg", "l51/6.jpg"]
+  },
+  {
+    id: "lingerie-52",
+    code: "0203", // كود المنتج الأصلي (للمطابقة مع شيت الأونر)
+    nameAr: "", // TODO: يتملى من الشيت
+    nameEn: "", // TODO
+    price: 0, // TODO
+    size: "",
+    description: "",
+    images: ["l52/1.jpg", "l52/2.jpg", "l52/3.jpg", "l52/4.jpg", "l52/5.jpg", "l52/6.jpg", "l52/7.jpg"]
+  },
+  {
+    id: "lingerie-53",
+    code: "0217", // كود المنتج الأصلي (للمطابقة مع شيت الأونر)
+    nameAr: "", // TODO: يتملى من الشيت
+    nameEn: "", // TODO
+    price: 0, // TODO
+    size: "",
+    description: "",
+    images: ["l53/1.jpg", "l53/2.jpg", "l53/3.jpg", "l53/4.jpg", "l53/5.jpg"]
+  },
+  {
+    id: "lingerie-54",
+    code: "0246", // كود المنتج الأصلي (للمطابقة مع شيت الأونر)
+    nameAr: "", // TODO: يتملى من الشيت
+    nameEn: "", // TODO
+    price: 0, // TODO
+    size: "",
+    description: "",
+    images: ["l54/1.jpg", "l54/2.jpg", "l54/3.jpg", "l54/4.jpg", "l54/5.jpg"]
+  },
+  {
+    id: "lingerie-55",
+    code: "0183", // كود المنتج الأصلي (للمطابقة مع شيت الأونر)
+    nameAr: "", // TODO: يتملى من الشيت
+    nameEn: "", // TODO
+    price: 0, // TODO
+    size: "",
+    description: "",
+    images: ["l55/1.jpg", "l55/2.jpg", "l55/3.jpg", "l55/4.jpg", "l55/5.jpg", "l55/6.jpg"]
+  },
+  {
+    id: "lingerie-56",
+    code: "0245", // كود المنتج الأصلي (للمطابقة مع شيت الأونر)
+    nameAr: "", // TODO: يتملى من الشيت
+    nameEn: "", // TODO
+    price: 0, // TODO
+    size: "",
+    description: "",
+    images: ["l56/1.jpg", "l56/2.jpg", "l56/3.jpg", "l56/4.jpg", "l56/5.jpg", "l56/6.jpg", "l56/7.jpg", "l56/8.jpg"]
+  },
+  {
+    id: "lingerie-57",
+    code: "012", // كود المنتج الأصلي (للمطابقة مع شيت الأونر)
+    nameAr: "", // TODO: يتملى من الشيت
+    nameEn: "", // TODO
+    price: 0, // TODO
+    size: "",
+    description: "",
+    images: ["l57/1.jpg", "l57/2.jpg", "l57/3.jpg", "l57/4.jpg", "l57/5.jpg", "l57/6.jpg", "l57/7.jpg"]
+  },
+  {
+    id: "lingerie-58",
+    code: "0243", // كود المنتج الأصلي (للمطابقة مع شيت الأونر)
+    nameAr: "", // TODO: يتملى من الشيت
+    nameEn: "", // TODO
+    price: 0, // TODO
+    size: "",
+    description: "",
+    images: ["l58/1.jpg", "l58/2.jpg", "l58/3.jpg", "l58/4.jpg", "l58/5.jpg", "l58/6.jpg", "l58/7.jpg"]
+  },
+  {
+    id: "lingerie-59",
+    code: "0242", // كود المنتج الأصلي (للمطابقة مع شيت الأونر)
+    nameAr: "", // TODO: يتملى من الشيت
+    nameEn: "", // TODO
+    price: 0, // TODO
+    size: "",
+    description: "",
+    images: ["l59/1.jpg", "l59/2.jpg", "l59/3.jpg", "l59/4.jpg", "l59/5.jpg"]
+  },
+  {
+    id: "lingerie-60",
+    code: "031", // كود المنتج الأصلي (للمطابقة مع شيت الأونر)
+    nameAr: "", // TODO: يتملى من الشيت
+    nameEn: "", // TODO
+    price: 0, // TODO
+    size: "",
+    description: "",
+    images: ["l60/1.jpg", "l60/2.jpg", "l60/3.jpg", "l60/4.jpg", "l60/5.jpg", "l60/6.jpg", "l60/7.jpg", "l60/8.jpg"]
+  },
+  {
+    id: "lingerie-61",
+    code: "058", // كود المنتج الأصلي (للمطابقة مع شيت الأونر)
+    nameAr: "", // TODO: يتملى من الشيت
+    nameEn: "", // TODO
+    price: 0, // TODO
+    size: "",
+    description: "",
+    images: ["l61/1.jpg", "l61/2.jpg", "l61/3.jpg", "l61/4.jpg", "l61/5.jpg", "l61/6.jpg", "l61/7.jpg", "l61/8.jpg"]
+  },
+  {
+    id: "lingerie-62",
+    code: "0181", // كود المنتج الأصلي (للمطابقة مع شيت الأونر)
+    nameAr: "", // TODO: يتملى من الشيت
+    nameEn: "", // TODO
+    price: 0, // TODO
+    size: "",
+    description: "",
+    images: ["l62/1.jpg", "l62/2.jpg", "l62/3.jpg", "l62/4.jpg", "l62/5.jpg"]
+  },
+];
